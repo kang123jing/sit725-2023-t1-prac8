a41bcd619257918013b6e53a1d40d07bce57b9d1
stephenliu8964 <dq89_64@hotmail.com>
2024-03-26 22:55:10 +1100

    adding db

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,7 @@
-const cardList = [
-    {
-        title: 'Kitten 2', image: 'images/kitten-2.png', link: 'About Kitten 2', description: 'Kitten 2 description'
-    },
-    {
-        title: 'Kitten 3', image: 'images/kitten-3.png', link: 'About Kitten 3', description: 'Kitten 3 description'
-    }
-];
-
 const addCards = (items) => {
     items.forEach(item => {
         let itemToAppend = '<div class="col s4 center-align">'+
-        '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
+        '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.path+'">'+
         '</div><div class="card-content">'+
         '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
         '<div class="card-reveal">'+
@@ -23,17 +14,38 @@ const addCards = (items) => {
 
 function submitForm() {
     let formData = {};
-    formData.first_name = $('#first_name').val();
-    formData.last_name = $('#last_name').val();
-    formData.password = $('#password').val();
-    formData.email = $('#email').val();
+    formData.title = $('#title').val();
+    formData.path = $('#path').val();
+    formData.link = $('#link').val();
+    formData.description = $('#description').val();
     
     console.log("Form Data Submitted: ", formData);
+    postCat(formData);
+}
+
+function getCats() {
+    $.get('api/cats', (response) => {
+        console.log(response.data);
+        if (response.data) {
+            addCards(response.data);
+        }
+    })
+}
+
+function postCat(cat) {
+    $.ajax({
+        url: 'api/cat',
+        data: cat,
+        type: 'POST',
+        success: (result) => {
+            console.log(result.data);
+        }
+    })
 }
 
 $(document).ready(function(){
     $('.materialboxed').materialbox();
-    addCards(cardList);
+    getCats();
     $('.modal').modal();
     $('#formSubmit').click(()=>{
         submitForm();
